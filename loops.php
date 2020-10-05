c28b1e046d46f2b7ec6ec09ebe3a4cc708a3ebfb
Muhammad Adil Kamal <Name.ak47@gmail.com>
2020-10-05 10:44:30 +0500

    working on loops (for,while,do-while,foreach) and jump statments

---
+++ loops.php
@@ -0,0 +1,73 @@
+<?php
+
+
+for($i=0; $i<=10 ;$i++ )
+{
+	
+	if($i==5)
+	{
+	//	break;
+		exit();
+	}
+	
+//	if($i%2==0)
+//	{
+//		//break;
+//		
+//		continue;
+//	}
+	echo "$i<br>";
+	
+	
+}
+
+echo "-----------Program completed------------";;
+
+//for($i=10; $i>=0 ;$i-- )
+//{
+//	echo "$i<br>";
+//}
+
+//$i=0;
+//while($i<=10)
+//{
+//	
+//	echo $i."<br>";
+//	
+//}
+
+//$i=1;
+//do
+//{
+//	echo $i."<br>";
+//	
+//}while(false);
+
+//$names=array("Irtaza","Waleed","Ghani","Zain","Yasir","Zaid","Khizar","Iftikhar","Rabia","Huma Wania shaikh","Sarosh","Zaheen Fatima","Maryam");
+//
+//foreach($names as $n)
+//{
+//	echo $n."<br>";
+//}
+
+
+// Recursion
+//$i=1;
+//function M()
+//{
+//	global $i;
+//	echo $i."<br>";
+//	$i++;
+//	if($i<=100)
+//	{
+//		M();
+//	}
+//	
+//}
+//
+//M();
+
+
+
+
+?>
