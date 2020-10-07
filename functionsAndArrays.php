3f6496245402152de7917f76cefde53492f0a8a2
Muhammad Adil Kamal <Name.ak47@gmail.com>
2020-10-07 11:04:20 +0500

    working with functions and arrays

---
+++ functionsAndArrays.php
@@ -0,0 +1,122 @@
+<?php
+
+//error_reporting(0);
+//// Mathematical Functions
+//
+////$a=-1;
+//
+//echo abs($a);
+//echo "<hr>";
+//
+//echo max(21,54,32,65,897,54,21);
+//echo "<hr>";
+//$marks=array(21,54,32,65,897,54,21);
+//
+//echo min($marks);
+//echo "<hr>";
+//echo round(2.6);
+//
+//echo "<hr>";
+//echo round(2.4);
+//echo "<hr>";
+//echo ceil(2.4);
+//echo "<hr>";
+//echo floor(2.9);
+//echo "<hr>";
+//
+//for($i=0;$i<=300;$i++)
+//{
+//	echo chr($i);
+//}
+
+//function SumOfSquare($a,$b)
+//{
+//	function square($x)
+//	{
+//		return $x*$x;
+//	}
+//	
+//	return square($a)+square($b);
+//}
+//
+//
+//echo SumOfSquare(10,50);  
+
+
+
+
+// Single dimensional Array
+
+/// this is index array
+//$phoneNumbers=array("0320-9865321","0333-7414712","0320-9865748");
+
+
+// this is Assosiative array
+//$phoneNumbers=array("Ali"=>"0320-9865321","amjad"=>"0333-7414712","malik"=>"0320-9865748");
+//
+//foreach($phoneNumbers as $key => $number)
+//{
+//	echo "$key : $number<br>";
+//}
+
+
+// find Factorial
+
+
+//$f=4;
+//$a=1;
+//
+//for($i=1;$i<=$f;$i++)
+//{
+//	$a=$a*$i;
+//}
+
+$f=4;
+$a=1;
+for($i=1;$i<$f;$i++,$a*=$i);
+
+echo "Factorial of $f = $a";
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
